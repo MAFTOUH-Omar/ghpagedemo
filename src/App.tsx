db429c855e92175523ea90a9197af28c1851785d
MAFTOUH <omar.maftou0000@gmail.com>
2024-11-18 12:32:45 +0100

    Add deployment workflow03

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more Omar
+        Click on the Vite and React logos to learn more Omar up
       </p>
     </>
   )
